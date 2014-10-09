9dfa48aba858a85905bfcc7bebafa173f65b0446
Keith Smith <ksmith.ut@gmail.com>
2014-10-09 15:52:33 -0600

    added lib files to read webcam data and split up and analyze the data

---
+++ test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var fs = require('fs');
+
+var webcamRegions = require('./lib/webcam-regions');
+
+var regions = [
+  {
+    width: 100,
+    height: 200,
+    x: 100,
+    y: 200
+  },
+  {
+    width: 200,
+    height: 100,
+    x: 500,
+    y: 500
+  }
+];
+
+webcamRegions(regions, function (err, images, main) {
+  if (err) { return console.error(err); }
+  //fs.writeFile('all.jpg', main, function () {});
+  images.map(function (img, index) {
+    //fs.writeFile(index + '.jpg', img.content, function () {});
+  });
+});
