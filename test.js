730a6879f19808969f9176577374a70f21f474d8
Keith Smith <ksmith.ut@gmail.com>
2014-10-14 17:04:27 -0600

    added servo lib stuff

--- test.js
+++ test.js
@@ -6,23 +6,39 @@ var webcamRegions = require('./lib/webcam-regions');
 
 var regions = [
   {
-    width: 100,
-    height: 200,
-    x: 100,
-    y: 200
+    width: 146,
+    height: 146,
+    x: 289,
+    y: 360
   },
   {
-    width: 200,
-    height: 100,
-    x: 500,
-    y: 500
+    width: 146,
+    height: 146,
+    x: 607,
+    y: 364
   }
 ];
 
-webcamRegions(regions, function (err, images, main) {
-  if (err) { return console.error(err); }
-  fs.writeFile('all.jpg', main, function () {});
-  images.map(function (img, index) {
-    fs.writeFile(index + '.jpg', img.content, function () {});
-  });
+
+
+require('./lib/servo')({
+  pin: 9,
+  startAt: 120,
+  range: [0, 120]
+}, function (servo) {
+  servo.to(120).stop();
+
+  setTimeout(function () {
+    servo.to(0);
+    webcamRegions(regions, function (err, images, main) {
+      if (err) { return console.error(err); }
+      console.log(images[0].color);
+      console.log(images[1].color);
+      setTimeout(function () {
+        servo.to(120).stop();
+      }, 5000);
+    });
+  }, 5000);
+
+
 });
