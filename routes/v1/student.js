229221ca0be46341945f8d2a80dd98e76d8a97bf
Keith Smith <ksmith.ut@gmail.com>
2014-11-04 08:04:54 -0700

    finished up student api

--- routes/v1/student.js
+++ routes/v1/student.js
@@ -2,15 +2,17 @@
 
 var router  = require('express').Router();
 var Student = require('mongoose').model('Student');
+var auth   = require('rute')('middleware/auth');
 
 router.param('uvid', function (req, res, next, uvid) {
-  Student.findBy({uvid: uvid}).exec()
+  Student.findOne({uvid: uvid}).exec()
     .then(function (student) {
       if (student) { return student; }
       return Student.create({uvid: uvid});
     })
     .then(function (student) {
       req.student = student;
+      next();
     }, next);
 });
 
@@ -24,5 +26,44 @@ router.post('/:uvid/login', function (req, res) {
     });
   });
 });
+
+router.get('/current', function (req, res) {
+  res.json({
+    success: Boolean(req.session.student),
+    student: req.session.student
+  });
+});
+
+router.post('/logout', function (req, res) {
+  delete req.session.student;
+  req.session.save(function (err) {
+    if (err) { return next(err); }
+    res.json({ success: true });
+  });
+});
+
+router.route('/')
+  .get(
+    auth.hasAccess('admin'),
+    function (req, res, next) {
+      Student.find({}).exec().then(function (students) {
+        res.json({
+          success: true,
+          students: students
+        });
+      }, next);
+    }
+  );
+
+router.route('/:uvid')
+  .delete(
+    auth.hasAccess('admin'),
+    function (req, res, next) {
+      if (!req.student) { return next(); }
+      req.student.remove(function () {
+        res.json({ success: true });
+      });
+    }
+  );
 
 module.exports = router;
---
+++ test/student.js
@@ -0,0 +1,171 @@
+'use strict';
+/* jshint maxlen: false */
+/* jshint maxstatements: false */
+
+require('../index');
+
+var mongoose = require('mongoose');
+var jar      = require('request').jar();
+var request  = require('request').defaults({jar: jar});
+var expect   = require('expect.js');
+var config   = require('configly').config;
+
+var baseUrl  = 'http://localhost:' + config.get('env.port');
+var User, user, csrfToken;
+var defaultUser = {
+  email: 'test@email.com',
+  password: 'testingtesting',
+  role: 'admin'
+};
+var testUvid = '10000000';
+
+// API endpoints :
+//   /routes/v1/auth.js
+//     POST   /v1/auth/login
+//     ANY    /v1/auth/logout
+//     GET    /v1/auth/roles
+//     POST   /v1/auth/request-reset
+//     POST   /v1/auth/reset-password
+//   /routes/v1/user.js
+//     GET    /v1/user
+//     GET    /v1/user/:id
+//     POST   /v1/user
+//     PUT    /v1/user/:id
+//     DELETE /v1/user/:id
+
+describe('user tests', function () {
+
+  before(function (done) {
+    User = mongoose.model('User');
+    User.defaultUser(defaultUser).then(function (theUser) {
+      user = theUser;
+      request.get(baseUrl, function (err, res, body) {
+        csrfToken = jar.getCookies(baseUrl)[0].value;
+        done();
+      });
+    });
+  });
+
+  after(function (done) {
+    // Drop the test database after the test is done.
+    mongoose.connection.db.dropDatabase(done);
+  });
+
+  describe('Level 1', function () {
+
+    it('should be able to login', function (done) {
+      request.post({
+        url: baseUrl + '/v1/auth/login',
+        json: {
+          email: defaultUser.email,
+          password: defaultUser.password,
+          _csrf: csrfToken
+        }
+      }, function (err, res, body) {
+        expect(err).to.be(null);
+        expect(body).to.be.an(Object);
+        expect(body.success).to.be(true);
+        expect(body.user).to.be.an(Object);
+        expect(body.user.id).to.be(user.id);
+        user = body.user;
+        done();
+      });
+    });
+
+    it('should login as student', function (done) {
+      request.post({
+        url: baseUrl + '/v1/student/' + testUvid + '/login',
+        json: {
+          _csrf: csrfToken
+        }
+      }, function (err, res, body) {
+        expect(err).to.be(null);
+        expect(body).to.be.an(Object);
+        expect(body.success).to.be(true);
+        expect(body.student).to.be.an(Object);
+        expect(body.student.uvid).to.be(testUvid);
+        done();
+      });
+    });
+
+    it('should get currently logged in student', function (done) {
+      request.get(baseUrl + '/v1/student/current', function (err, res, body) {
+        expect(err).to.be(null);
+        body = JSON.parse(body);
+        expect(body).to.be.an(Object);
+        expect(body.success).to.be(true);
+        expect(body.student).to.be.an(Object);
+        expect(body.student.uvid).to.be(testUvid);
+        done();
+      });
+    });
+
+    it('should be able to logout', function (done) {
+      request.post({
+        url: baseUrl + '/v1/student/logout',
+        json: {
+          _csrf: csrfToken
+        }
+      }, function (err, res, body) {
+        expect(err).to.be(null);
+        expect(body).to.be.an(Object);
+        expect(body.success).to.be(true);
+
+        request.get(baseUrl + '/v1/student/current', function (err, res, body) {
+          expect(err).to.be(null);
+          body = JSON.parse(body);
+          expect(body).to.be.an(Object);
+          expect(body.success).to.be(false);
+          expect(body.student).to.be(undefined);
+          done();
+        });
+
+      });
+    });
+
+    it('should allow admins to get list of students', function (done) {
+      request.get(baseUrl + '/v1/student/', function (err, res, body) {
+        expect(err).to.be(null);
+        body = JSON.parse(body);
+        expect(body).to.be.an(Object);
+        expect(body.success).to.be(true);
+        expect(body.students).to.be.an(Array);
+        expect(body.students.length).to.be(1);
+        expect(body.students[0].uvid).to.be(testUvid);
+        done();
+      });
+    });
+
+    it('should delete a student', function (done) {
+      request.del({
+        url: baseUrl + '/v1/student/' + testUvid,
+        json: {
+          _csrf: csrfToken
+        }
+      }, function (err, res, body) {
+        expect(err).to.be(null);
+        expect(body).to.be.an(Object);
+        expect(body.success).to.be(true);
+
+        request.get(baseUrl + '/v1/student/', function (err, res, body) {
+          expect(err).to.be(null);
+          body = JSON.parse(body);
+          expect(body).to.be.an(Object);
+          expect(body.success).to.be(true);
+          expect(body.students).to.be.an(Array);
+          expect(body.students.length).to.be(0);
+          done();
+        });
+
+      });
+    });
+
+  });
+
+  describe('Level 2', function (done) {
+
+
+
+  });
+
+});
