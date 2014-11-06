912668e88e5b51999f6fc5c29a4388702c172fc8
Keith Smith <ksmith.ut@gmail.com>
2014-11-06 14:29:17 -0700

    added question routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,5 +5,6 @@ var router = require('express').Router();
 router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
 router.use('/student', require('./student'));
+router.use('/question', require('./question'));
 
 module.exports = router;
--- routes/v1/question.js
+++ routes/v1/question.js
@@ -11,22 +11,56 @@ router.get('/types', function (req, res, next) {
   });
 });
 
+router.use(auth.hasAccess('admin'));
+
+router.param('id', function (req, res, next, id) {
+  Question.findById(id).exec().then(function (question) {
+    req.question = question;
+    next();
+  });
+});
+
+router.route('/:id')
+  // GET /v1/question/:id
+  .get(function (req, res, next) {
+    if (!req.question) { return next(); }
+    res.json({success: true, question: req.question});
+  })
+  // PUT /v1/question/:id
+  .put(function (req, res, next) {
+    if (!req.question) { return next(); }
+    delete req.body._id;
+    Object.keys(req.body).map(function (key) {
+      req.question[key] = req.body[key];
+    });
+    req.question.save(function (err, question) {
+      if (err) { return next(err); }
+      res.json({success: true, question: question});
+    });
+  })
+  // DELETE /v1/question/:id
+  .delete(function (req, res, next) {
+    if (!req.question) { return next(); }
+    req.question.remove(function (err, question) {
+      if (err) { return next(err); }
+      res.json({success: true});
+    });
+  });
+
 router.route('/')
-  .post(
-    auth.hasAccess('admin'),
-    function (req, res, next) {
-      Question.create(req.body).then(function (question) {
-        res.json({
-          success: true,
-          question: question
-        });
-      }, function (err) {
-        res.json({
-          success: false,
-          err: err.message
-        });
-      });
-    }
-  );
+  // GET /v1/question
+  .get(function (req, res, next) {
+    Question.find({}).exec().then(function (questions) {
+      res.json({ success: true, questions: questions })
+    });
+  })
+  // POST /v1/question
+  .post(function (req, res, next) {
+    Question.create(req.body).then(function (question) {
+      res.json({ success: true, question: question });
+    }, function (err) {
+      res.json({ success: false, err: err.message });
+    });
+  });
 
 module.exports = router;
