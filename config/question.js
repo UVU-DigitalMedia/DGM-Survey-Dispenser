0f6ba77b96b2a70d596587f07acfa15e28386b11
ksmithut <ksmith.ut@gmail.com>
2014-12-17 12:45:30 -0700

    removed unsupported questions

--- config/question.js
+++ config/question.js
@@ -22,29 +22,29 @@ module.exports = {
         return typeof value === 'boolean';
       }
     },
-    shortAnswer: {
-      label: 'Short Answer',
-      validate: function (value) {
-        return Boolean(value);
-      }
-    },
-    essay: {
-      label: 'Essay',
-      validate: function (value) {
-        return Boolean(value);
-      }
-    },
-    ordering: {
-      label: 'Ordering',
-      validate: function (value, choices) {
-        if (!Array.isArray(value) && value.length !== choices.length) {
-          return false;
-        }
-        return value.every(function (value) {
-          return validateMultipleChoice(value, choices);
-        });
-      }
-    },
+    // shortAnswer: {
+    //   label: 'Short Answer',
+    //   validate: function (value) {
+    //     return Boolean(value);
+    //   }
+    // },
+    // essay: {
+    //   label: 'Essay',
+    //   validate: function (value) {
+    //     return Boolean(value);
+    //   }
+    // },
+    // ordering: {
+    //   label: 'Ordering',
+    //   validate: function (value, choices) {
+    //     if (!Array.isArray(value) && value.length !== choices.length) {
+    //       return false;
+    //     }
+    //     return value.every(function (value) {
+    //       return validateMultipleChoice(value, choices);
+    //     });
+    //   }
+    // },
   }
 };
 
