1820cff424e0f90345d069dc513e0b14fc0bc46c
ksmithut <ksmith.ut@gmail.com>
2015-03-17 22:56:27 -0600

    added 404 component

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,23 +1,26 @@
 'use strict';
 
-var React     = require('react');
-var Router    = require('react-router');
+var React        = require('react');
+var Router       = require('react-router');
 
-var Route = Router.Route;
+var Route        = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
 
-var App       = require('./App.js');
-var LoginForm = require('./views/LoginForm/LoginForm');
-var Dashboard = require('./views/Dashboard/Dashboard');
-var Users     = require('./views/Users/Users');
-var User      = require('./views/Users/User');
+var App          = require('./App.js');
+var LoginForm    = require('./views/LoginForm/LoginForm');
+var Dashboard    = require('./views/Dashboard/Dashboard');
+var Users        = require('./views/Users/Users');
+var User         = require('./views/Users/User');
+var FourOhFour   = require('./views/Static/FourOhFour');
 
 module.exports = (
   <Route name="app" path="/" handler={App}>
     <Route name="login" path="/login" handler={LoginForm} />
     <Route name="dashboard" path="/dashboard" handler={Dashboard}>
       <Route name="users" path="/dashboard/users" handler={Users}>
-        <Route name="user" path="/dashboard/users/:userId" handler={User} />
+        <Route name="user" path="/dashboard/users/:userId" handler={User} ignoreScrollBehavior={true}/>
       </Route>
     </Route>
+    <DefaultRoute handler={FourOhFour} />
   </Route>
 );
