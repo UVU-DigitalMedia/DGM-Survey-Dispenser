7ae691e6c43ce32ffe39d629e8ffd24657df4972
ksmithut <ksmith.ut@gmail.com>
2015-03-31 19:29:53 -0600

    updated servo to have delay

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var PIN   = 9;
+var PIN   = 10;
 var servo = require('./lib/servo').bind(null, PIN);
 
-servo(180);
+console.log('hello');
 servo(0);
 servo(180);
-servo(360);
-servo(180);
+servo(0);
+servo(undefined);
